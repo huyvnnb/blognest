2cc8b0896de0ac949d40b412f8ef451e0290961d
huyvnnb <huyqktk@gmail.com>
2025-02-26 08:56:11 +0700

    feat: user crud

--- src/main/java/com/huyvnnb/blognest/dto/response/UserResponse.java
+++ src/main/java/com/huyvnnb/blognest/dto/response/UserResponse.java
@@ -1,16 +1,16 @@
 package com.huyvnnb.blognest.dto.response;
 
-import com.huyvnnb.blognest.util.Role;
+import com.huyvnnb.blognest.enums.Role;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserResponse {
+    Long id;
     String email;
     String password;
     String firstName;
